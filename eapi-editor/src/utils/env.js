0a2b4efb2fe196217e43b2f4b2979c07a166add2
liuzhiyong <liuzyhn@sina.cn>
2018-09-19 16:44:58 +0800

    环境变量等添加

--- eapi-editor/src/utils/env.js
+++ eapi-editor/src/utils/env.js
@@ -2,16 +2,7 @@
  * 配置编译环境和线上环境之间的切换
  */
 
-let baseUrl = 'http://localhost:7050'; // 接口域名
-let context = '/v2/api-docs';
-let env = process.env.SERVER_ENV;
-if (env === 'dev') { // 开发环境，接口地址为测试环境地址
-  baseUrl = 'http://localhost:7050';
-} else if (env === 'test') { // 测试环境
-  // baseUrl = 'http://10.133.255.201:7050';
-} else if (env === 'pre') { // 预生产环境
-  // baseUrl = 'https://api-pre.meimeitech.com';
-} else if (env === 'prod') { // 生产环境
-  // baseUrl = 'https://api.meimeitech.com';
-}
+let baseUrl = process.env.VUE_APP_SERVER_URL; // 接口域名
+let context = process.env.VUE_APP_CONTEXT;
+
 export {baseUrl, context};
