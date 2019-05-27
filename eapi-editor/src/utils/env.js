7a68163eacbff17d1480c340a3ddc9f61ddbea87
Shawn <lishuai_247@163.com>
2019-05-27 10:29:07 +0800

    升级依赖包

--- eapi-editor/src/utils/env.js
+++ eapi-editor/src/utils/env.js
@@ -2,7 +2,7 @@
  * 配置编译环境和线上环境之间的切换
  */
 
-let baseUrl = process.env.VUE_APP_SERVER_URL; // 接口域名
-let context = process.env.VUE_APP_CONTEXT;
+const baseUrl = process.env.VUE_APP_SERVER_URL; // 接口域名
+const context = process.env.VUE_APP_CONTEXT;
 
 export {baseUrl, context};
