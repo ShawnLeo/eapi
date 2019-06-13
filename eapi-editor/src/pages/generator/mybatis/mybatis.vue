6fec9b7702b17314f3d2a9abf70dbd8114f3a721
Shawn <lishuai_247@163.com>
2019-06-13 16:25:54 +0800

    数据库表搜索

--- eapi-editor/src/pages/generator/mybatis/mybatis.vue
+++ eapi-editor/src/pages/generator/mybatis/mybatis.vue
@@ -3,12 +3,20 @@
 		<!--<mybatis-fragment v-if="currView=='see'" @close="currView='index'" :column="column" :table="table" style="min-height: 400px"/>-->
 		<Row>
 			<i-col span="24">
-				<Button @click="init" type="primary" icon="md-refresh" style="float: right;">刷新</Button>
+				<Form ref="formInline" inline style="float: right;">
+					<FormItem>
+						<Button @click="init" type="primary" icon="md-refresh" >刷新</Button>
+					</FormItem>
+
+					<FormItem prop="search">
+						<Input type="text" placeholder="名称搜索" v-model="searchModel" @on-keyup="searchData"/>
+					</FormItem>
+				</Form>
 			</i-col>
 		</Row>
 
 		<Row class="table-list">
-			<Table :loading="loading" border :columns="tableColumns" :data="tables" ref="table"></Table>
+			<Table :loading="loading" border :columns="tableColumns" :data="filterTables" ref="table"></Table>
 		</Row>
 
 		<mybatis-gen v-model="modalVisible" :table="table" :columns="columns" @on-cancel="modalVisible=false" @on-ok="modalVisible = false"></mybatis-gen>
@@ -29,6 +37,7 @@
 			return {
 				tables: [], // 表单数据
 				columnMaps: {},
+				searchModel: '',
 				table: {},
 				columns: [],
 				modalVisible: false,
@@ -41,6 +50,7 @@
 					},
 					{
 						title: "表名",
+						sortable: true,
 						key: "tableName"
 					},
 					{
@@ -114,6 +124,7 @@
 				generatorDatabaseAll(JSON.parse(db), (data) => {
 					this.columnMaps = data.body.column;
 					this.tables = data.body.table;
+					this.filterTables = data.body.table;
 					this.loading = false;
 				});
 
@@ -128,6 +139,9 @@
 					name: 'codeGeneratorMybatisTable',
 					params: {table: v, column: this.columnMaps[v.tableName]}
 				});
+			},
+			searchData() {
+				this.filterTables = this.tables.filter(item => item.tableName.toLowerCase().indexOf(this.searchModel.toLowerCase()) > -1);
 			}
 		},
 		mounted() {
@@ -139,9 +153,5 @@
 <style scoped>
 	.mybatis {
 		padding: 20px;
-	}
-
-	.mybatis .table-list {
-		margin-top: 15px;
 	}
 </style>
